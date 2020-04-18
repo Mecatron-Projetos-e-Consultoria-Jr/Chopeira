448faa012bc3e113dcb99522f1ea2281bbad4361
Pedro Cruz <pedrohlcruz@gmail.com>
2020-04-18 19:58:07 -0300

    First pre tested and first iteration of the compressor library

---
+++ compressor.h
@@ -0,0 +1,26 @@
+class compressor {
+
+    private:
+        int pin;
+    
+    public:
+    
+        compressor(int digital_pin){
+
+            pinMode(digital_pin,OUTPUT);
+            pin = digital_pin;
+        }
+
+        void turn_on_cooling(){
+
+            // Close the relay (so the cooler turns on)
+            digitalWrite(pin,HIGH);
+        }
+
+        void turn_off_cooling(){
+
+            // Open the relay
+            digitalWrite(pin,LOW);
+        }
+
+};
