54e0c8f4553a3c94f7855d847a4502857a93bb10
Pedro Cruz <pedrohlcruz@gmail.com>
2020-04-20 21:27:54 -0300

    Added the button class and implemented its color function

--- choppeira.ino
+++ choppeira.ino
@@ -1,11 +1,23 @@
-#include "temperature_sensor.h"
 #include "compressor.h"
 #include "solenoid.h"
+#include "button.h"
 
-// instantiate temperature_sensor and attach the signal pin to the analog GPIO A0 and instantiate compressor to control cooling system and attach it to GPIO 9 / instantiate the selonoide class and attach it to pin 10 on the arduino 
-temperature_sensor sensor(A0);
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is connected to the Arduino digital pin 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// instantiate compressor to control cooling system and attach it to GPIO 9 / instantiate the selonoide class and attach it to pin 10 on the arduino  / instance the button class and Set the data pin to 13, red led to analof A0, green to analog A1, blue to analog A2
 compressor cooling_system(9);
 solenoid  valve(10);
+button power_button(13,A0,A1,A2);
 
 // Set the target temperature for the liquid
 float target_temperature = 0.0f;
@@ -18,20 +30,29 @@ void setup(){
     // Set the pin mode for the button 
     pinMode(button,INPUT);
 
+    // Start the emperature sensor 
+    sensors.begin();
+
     // close the vaulve to avoid leaking
     valve.close_valve();
 }
 
 void loop(){
 
-    // Check the temperature 
-    float current_temperature = sensor.get_current_temperature();
+    // Resquest the temperature for all the sensors connected to the arduino 
+    sensors.requestTemperatures(); 
+
+    // Check the temperature  - by index means it's getting the data for the first sensor (if in the future more sensores are added) -- index starts at 0
+    float current_temperature = sensors.getTempCByIndex(0);
 
     // If the temperature is higher than the target, turn on the cooling system
     if (current_temperature > target_temperature){
         
         // turn on the cooling system
         cooling_system.turn_on_cooling();
+
+        // Turn the LED to red 
+        power_button.set_color('R');
     }
 
     // If the temperature is lower than the target, turn off the cooling system
@@ -39,24 +60,22 @@ void loop(){
 
         // turn off the cooling system 
         cooling_system.turn_off_cooling();
+
+        // Set the button color to green 
+        power_button.set_color('G');
     }
     
-    // Check to see if the button is pressed 
-    bool button_pressed = digitalRead(button);
 
-    while (button_pressed && current_temperature <= target_temperature)
+    while (power_button.is_pressed() && current_temperature <= target_temperature)
     {
         // open the valve 
         valve.open_valve();
 
-        // update the button state 
-        button_pressed = digitalRead(button);
-
         // update the temperature value 
-        current_temperature = sensor.get_current_temperature();
+        current_temperature = sensors.getTempCByIndex(0);
         
         // if the button stopped being pressed, close the valve 
-        if(!button_pressed){
+        if(!power_button.is_pressed()){
 
             // close the valve 
             valve.close_valve();
