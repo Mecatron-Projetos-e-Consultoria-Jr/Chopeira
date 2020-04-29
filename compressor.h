f424a83513df0d48a0ae1ed7269a752c1cf5170f
Pedro Cruz <pedrohlcruz@gmail.com>
2020-04-29 12:42:30 -0300

    Added print statements

--- compressor.h
+++ compressor.h
@@ -1,4 +1,4 @@
-// #define log(x) Serial.println(x)
+#define log(x) Serial.println(x)
 
 class compressor {
 
@@ -17,14 +17,14 @@ class compressor {
 
             // Close the relay (so the cooler turns on)
             digitalWrite(pin,HIGH);
-            Serial.println("[compressor.h]Setting cooling to HIGH");
+            log("[compressor.h]Setting cooling to HIGH");
         }
 
         void turn_off_cooling(){
 
             // Open the relay
             digitalWrite(pin,LOW);
-            Serial.println("[compressor.h]Setting cooling to LOW");
+            log("[compressor.h]Setting cooling to LOW");
         }
 
 };
