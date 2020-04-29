5c808cdebeac05f9db3ba60a1a03af17ee411b4b
Pedro Cruz <pedrohlcruz@gmail.com>
2020-04-29 12:43:10 -0300

    Added print statements

--- solenoid.h
+++ solenoid.h
@@ -1,4 +1,4 @@
-// #define log(x) Serial.println(x)
+#define log(x) Serial.println(x)
 
 class solenoid {
 
@@ -10,20 +10,21 @@ class solenoid {
         solenoid(int output_pin){
             pin = output_pin;
             pinMode(pin,OUTPUT);
+          
         }
 
         void open_valve(){
 
             // Open the valve so the liquid comes out 
             digitalWrite(pin,HIGH);
-            Serial.println("[solenoid.h]Setting solenoid to HIGH");
+            log("[solenoid.h]Setting solenoid to HIGH");
         }
 
         void close_valve(){
 
             // Close the valve 
             digitalWrite(pin,LOW);
-            Serial.println("[solenoid.h]Setting solenoid to LOW");
+            log("[solenoid.h]Setting solenoid to LOW");
         }
 
 };
